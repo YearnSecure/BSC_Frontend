a7d26e3b4842c9c7c6a669291466e3faeef9a14b
DevElem <dev1@ysec.finance>
2021-04-25 02:06:01 +0200

    differntiate between wc and meta + wc web3 working on reads +todo writes

--- src/components/plugins/WalletConnection/WalletConnector.ts
+++ src/components/plugins/WalletConnection/WalletConnector.ts
@@ -1,18 +1,43 @@
-import WalletConnect from "@walletconnect/client";
-import QRCodeModal from "@walletconnect/qrcode-modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3 from "web3";
 export default class WalletConnector{
     private metamaskProvider: any
-    private connectorWalletConnect: WalletConnect;
     private metamaskConnected: boolean;
     private walletConnectConnected: boolean;
+    private tempWC: WalletConnectProvider;
 
     constructor(metaMaskProvider: any){
       this.metamaskProvider = metaMaskProvider;
-      this.connectorWalletConnect = new WalletConnect({
-        bridge: "https://bridge.walletconnect.org", // Required
-        qrcodeModal: QRCodeModal,
+      this.tempWC = new WalletConnectProvider({
+        rpc: {
+          97: "https://data-seed-prebsc-1-s1.binance.org:8545/"
+        },
+        //bridge: 'https://bridge.walletconnect.org',
+        //qrcode: true,
       });
+
+      // this.tempWC.on("open", () => {
+      //   console.log("open");
+      // });
+      
+      // // Subscribe to session disconnection/close
+      // this.tempWC.on("close", (code: number, reason: string) => {
+      //   console.log(code, reason);
+      // });
+
+      // this.tempWC.on("accountsChanged", (accounts: string[]) => {
+      //   console.log(accounts);
+      // });
+      
+      // // Subscribe to chainId change
+      // this.tempWC.on("chainChanged", (chainId: number) => {
+      //   console.log(chainId);
+      // });
+      
+      // // Subscribe to session disconnection
+      // this.tempWC.on("disconnect", (code: number, reason: string) => {
+      //   console.log(code, reason);
+      // });
       
       this.metamaskConnected = false;
       this.walletConnectConnected = false;
@@ -43,7 +68,11 @@ export default class WalletConnector{
   }
 
   IsWalletConnectConnected(){
-    return this.connectorWalletConnect.connected;
+    const isConnected = this.tempWC.connector.connected;
+    if(isConnected)
+      this.tempWC.enable();
+
+    return isConnected;
   }
 
   ConnectMetaMask(){
@@ -52,16 +81,32 @@ export default class WalletConnector{
   }
 
   ConnectWalletConnect(){
-    this.connectorWalletConnect.createSession();
+    this.tempWC.enable();
+  }
+
+  private GetProvider()
+  {
+    if(this.metamaskConnected)
+      return new Web3(this.metamaskProvider);
+    if(this.walletConnectConnected)
+    {      
+      return new Web3(this.tempWC as any);
+    }
+      
+    return null;
+  }
+
+  async GetAccounts()
+  {
+    const web3 = this.GetProvider();
+    if(web3 != null)
+    {
+      return await web3.eth.getAccounts();
+    }    
   }
 
   async GetPresaleData(abi: any, id: any, address: string){
-    let web3 = null;
-    if(this.metamaskConnected)
-      web3 = new Web3(this.metamaskProvider);
-    if(this.walletConnectConnected)
-      web3 = new Web3(new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545/'));
-    
+    const web3 = this.GetProvider();
     if(web3 != null)
     {
       const presaleContractInterface = new web3.eth.Contract(abi);
@@ -71,11 +116,7 @@ export default class WalletConnector{
   }
 
   async GetTokenSymbol(abi: any, address: string){
-    let web3 = null;
-    if(this.metamaskConnected)
-      web3 = new Web3(this.metamaskProvider);
-    if(this.walletConnectConnected)
-      web3 = new Web3(new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545/'));
+    const web3 = this.GetProvider();
     
     if(web3 != null)
     {
@@ -84,4 +125,107 @@ export default class WalletConnector{
       return await tokenContractInterface.methods.symbol().call();
     }
   }
+
+  async ClaimTokens(abi: any, id: any, address: string, account: any){
+    const web3 = this.GetProvider();
+    if(web3 != null)
+    {
+      try{
+        const presaleContractInterface = new web3.eth.Contract(abi);
+        presaleContractInterface.options.address = address;
+        await presaleContractInterface.methods.ClaimTokens(id).send({from: account});
+      }catch(e){
+        console.log('error: ' + e)
+      }
+      
+    }
+  }
 }
+
+// import WalletConnect from "@walletconnect/client";
+// import QRCodeModal from "@walletconnect/qrcode-modal";
+// import Web3 from "web3";
+// export default class WalletConnector{
+//     private metamaskProvider: any
+//     private connectorWalletConnect: WalletConnect;
+//     private metamaskConnected: boolean;
+//     private walletConnectConnected: boolean;
+
+//     constructor(metaMaskProvider: any){
+//       this.metamaskProvider = metaMaskProvider;
+//       this.connectorWalletConnect = new WalletConnect({
+//         bridge: "https://bridge.walletconnect.org", // Required
+//         qrcodeModal: QRCodeModal,
+//       });
+      
+//       this.metamaskConnected = false;
+//       this.walletConnectConnected = false;
+//     }
+
+//   IsConnected() {
+//     this.metamaskConnected = this.IsMetamaskConnected();
+//     this.walletConnectConnected = this.IsWalletConnectConnected();
+//     console.log('metamask connected: ' + this.metamaskConnected);
+//     console.log('wc connected: ' + this.walletConnectConnected);
+//     return this.metamaskConnected || this.walletConnectConnected;
+//   }
+
+//   IsMetamaskConnected(){
+//     if (this.metamaskProvider === undefined) 
+//       return false;
+
+//     if (!this.metamaskProvider.isMetaMask)
+//       return false;
+
+//     if(!this.metamaskProvider.isConnected())
+//       return false;
+    
+//     if(this.metamaskProvider._state.accounts.length == 0)
+//       return false;
+    
+//     return true;
+//   }
+
+//   IsWalletConnectConnected(){
+//     return this.connectorWalletConnect.connected;
+//   }
+
+//   ConnectMetaMask(){
+//     this.metamaskProvider
+//           .request({ method: 'eth_requestAccounts' });
+//   }
+
+//   ConnectWalletConnect(){
+//     this.connectorWalletConnect.createSession();
+//   }
+
+//   async GetPresaleData(abi: any, id: any, address: string){
+//     let web3 = null;
+//     if(this.metamaskConnected)
+//       web3 = new Web3(this.metamaskProvider);
+//     if(this.walletConnectConnected)
+//       web3 = new Web3(new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545/'));
+    
+//     if(web3 != null)
+//     {
+//       const presaleContractInterface = new web3.eth.Contract(abi);
+//       presaleContractInterface.options.address = address;
+//       return await presaleContractInterface.methods.Presales(id).call();
+//     }
+//   }
+
+//   async GetTokenSymbol(abi: any, address: string){
+//     let web3 = null;
+//     if(this.metamaskConnected)
+//       web3 = new Web3(this.metamaskProvider);
+//     if(this.walletConnectConnected)
+//       web3 = new Web3(new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545/'));
+    
+//     if(web3 != null)
+//     {
+//       const tokenContractInterface = new web3.eth.Contract(abi);
+//       tokenContractInterface.options.address = address;
+//       return await tokenContractInterface.methods.symbol().call();
+//     }
+//   }
+// }
